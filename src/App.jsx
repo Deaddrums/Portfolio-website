876901eedd1e4993ee4865c7ea793cf1c879079a
Deaddrums <jimhorvath@live.nl>
2026-08-17 11:11:22 +0200

    Working on portfolio version.
    Freelance work sections are put on //

--- src/App.jsx
+++ src/App.jsx
@@ -3,39 +3,73 @@ import Navbar from "./assets/components/Navbar/Navbar.jsx";
 import MovingBackground from "./assets/components/MovingBackground/MovingBackground.jsx";
 import HeroSection from "./assets/Pages/HeroSection/HeroSection.jsx";
 import MovingPortfolio from "./assets/helpers/MovingPortfolio/MovingPortfolio.jsx";
-// import {Routes, Route} from "react-router-dom";
+import ExperienceSection from "./assets/Pages/ExperienceSection/ExperienceSection.jsx";
+import Footer from "./assets/components/Footer/Footer.jsx";
+import HowIWorkSection from "./assets/Pages/HowIWorkSection/HowIWorkSection.jsx";
+import ContactSectionPortfolio from "./assets/Pages/ContactSectionPortfolio/ContactSectionPortfolio.jsx";
 
+// import {Routes, Route} from "react-router-dom";
 
 
 function App() {
 
 
-  return (
-    <>
+    return (
+        <>
 
-<MovingBackground />
-
-      <Navbar></Navbar>
+            {/*<MovingBackground/>*/}
 
 
+            <main
+                className="appWrapper"
+            >
+                <Navbar/>
+                <div
+                    className="heroSectionContainer"
+                >
+                    <section
+                        id="Hero"
+                    >
+                        <HeroSection/>
 
-<div
-className="heroSectionContainer"
->
-        <section
-            id="Hero"
-        >
-            <HeroSection/>
-        </section>
+                    </section>
 
-        <section
-        id="portfolioSum"
-        >
-            <MovingPortfolio/>
-        </section>
-    </div>
-    </>
-  )
+                    <section
+                        id="portfolioSum"
+                    >
+                        <MovingPortfolio/>
+                    </section>
+
+                    {/*<section*/}
+                    {/*    id="Experience"*/}
+                    {/*>*/}
+                    {/*    <ExperienceSection/>*/}
+
+                    {/*</section>*/}
+
+                    {/*<section*/}
+                    {/*id="HowIWork"*/}
+                    {/*>*/}
+                    {/*    <HowIWorkSection/>*/}
+                    {/*</section>*/}
+
+
+                    <section
+                    id="contact"
+                    >
+                        <ContactSectionPortfolio/>
+                    </section>
+
+                </div>
+
+            </main>
+
+
+            <Footer/>
+
+
+        </>
+    )
 }
 
 export default App
